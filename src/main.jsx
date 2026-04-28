5e18b27bd00b1bff568ddf34271a5a21333fae80
RahmatBagus <64652583+Lenka69@users.noreply.github.com>
2026-04-28 22:59:07 +0700

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-// import LogRocket from 'logrocket'; 
-// LogRocket.init('ganti_dengan_ID_LogRocket_Anda_disini'); 
+import LogRocket from 'logrocket';
+LogRocket.init('d9u5fq/tugas-lab');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
