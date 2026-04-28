a9bb50edc57206aeaaf8bacd457a7c4b17cb66f8
RahmatBagus <64652583+Lenka69@users.noreply.github.com>
2026-04-28 23:04:48 +0700

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import LogRocket from 'logrocket';
+
+
 LogRocket.init('d9u5fq/tugas-lab');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
